ff72eaab9b99ce318aa89b2788b453768a882c6d
Vlasenko Aleksandr <baralexx181972@gmail.com>
2023-01-23 16:23:48 +0200

    hw2 done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const list = document.querySelector('#ingredients');
+
+const markup = ingredients.map(ingredient => `<li class='item'>${ingredient}</li>`).join('');
+console.log(markup);
+list.innerHTML = markup;
+// for (let i of ingredients) {
+//   const vegetables = document.createElement('li');
+//   vegetables.className = 'item';
+//   console.log(vegetables);
+//   vegetables.textContent = ingredients[i];
+//   console.log(vegetables);
+// }
+
+// const heading = document.createElement('li');
+// console.log(heading); // <h1></h1>
+
+// heading.textContent = ingredients[1];
+// console.log(heading);
